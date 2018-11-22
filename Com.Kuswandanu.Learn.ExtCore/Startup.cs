5634495b3c0bc65c2d09d9c5b497d09357e5561a
kuswandanu <kuswandanu@gmail.com>
2018-11-22 10:14:42 +0700

    Setup Main Web Application

--- Com.Kuswandanu.Learn.ExtCore/Startup.cs
+++ Com.Kuswandanu.Learn.ExtCore/Startup.cs
@@ -2,19 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExtCore.WebApplication.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Com.Kuswandanu.Learn.ExtCore
 {
     public class Startup
     {
+        private string extensionsPath;
+
+        public Startup(IHostingEnvironment hostingEnvironment, ILoggerFactory loggerFactory)
+        {
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configurationRoot = configurationBuilder.Build();
+
+            this.extensionsPath = hostingEnvironment.ContentRootPath + configurationRoot["Extensions:Path"];
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddExtCore(this.extensionsPath);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +41,8 @@ namespace Com.Kuswandanu.Learn.ExtCore
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseExtCore();
 
             app.Run(async (context) =>
             {
