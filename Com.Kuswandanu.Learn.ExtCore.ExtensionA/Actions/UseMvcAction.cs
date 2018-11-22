7ba67af7bcf8ec207a8445db7cbe9dda41dfef9a
kuswandanu <kuswandanu@gmail.com>
2018-11-22 11:53:16 +0700

    Modify ExtensionA

---
+++ Com.Kuswandanu.Learn.ExtCore.ExtensionA/Actions/UseMvcAction.cs
@@ -0,0 +1,17 @@
+﻿using ExtCore.Mvc.Infrastructure.Actions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using System;
+
+namespace Com.Kuswandanu.Learn.ExtCore.ExtensionA.Actions
+{
+    public class UseMvcAction : IUseMvcAction
+    {
+        public int Priority => 1000;
+
+        public void Execute(IRouteBuilder routeBuilder, IServiceProvider serviceProvider)
+        {
+            routeBuilder.MapRoute(name: "Default", template: "{controller}/{action}", defaults: new { controller = "Default", action = "Index" });
+        }
+    }
+}
---
+++ Com.Kuswandanu.Learn.ExtCore.ExtensionA/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Kuswandanu.Learn.ExtCore.ExtensionA.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
